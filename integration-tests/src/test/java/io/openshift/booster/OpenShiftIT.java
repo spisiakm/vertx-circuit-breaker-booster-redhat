a2586c2d22496a7859759f8c7c07769f085d406b
Martin Spisiak <mspisiak@redhat.com>
2017-10-31 15:57:17 +0100

    name-service and greeting-service are now copied over to integration-tests target directory to make Arquillian Cube work. This is done right before the integration tests are run.

--- integration-tests/src/test/java/io/openshift/booster/OpenShiftIT.java
+++ integration-tests/src/test/java/io/openshift/booster/OpenShiftIT.java
@@ -18,6 +18,7 @@
 
 package io.openshift.booster;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.arquillian.cube.openshift.impl.enricher.RouteURL;
@@ -31,6 +32,7 @@ import io.vertx.core.json.JsonObject;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.restassured.RestAssured.get;
 
 /**
  * @author Martin Kouba
@@ -55,6 +57,18 @@ public class OpenShiftIT {
 
 	@RouteURL(GREETING_SERVICE_APP)
 	private URL greetingServiceUrl;
+
+	@Before
+	public void setup() {
+		await().pollInterval(1, TimeUnit.SECONDS).atMost(5, TimeUnit.MINUTES).until(() -> {
+			try {
+				return get(greetingServiceUrl.toExternalForm() + "health").getStatusCode() == 200
+						&& get(nameServiceUrl.toExternalForm() + "health").getStatusCode() == 200;
+			} catch (Exception ignored) {
+				return false;
+			}
+		});
+	}
 
 	@Test
 	public void testThatCircuitBreakerIsClosedByDefault() throws InterruptedException {
